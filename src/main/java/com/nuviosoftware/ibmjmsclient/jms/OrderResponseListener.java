24cf448776bb0eff7eb20e43b08a45b52764b4d9
dadhjo <72243012+dadhjo@users.noreply.github.com>
2021-05-11 14:43:22 -0500

    Update OrderResponseListener.java

--- src/main/java/com/nuviosoftware/ibmjmsclient/jms/OrderResponseListener.java
+++ src/main/java/com/nuviosoftware/ibmjmsclient/jms/OrderResponseListener.java
@@ -12,12 +12,26 @@ import javax.jms.TextMessage;
 @Component
 public class OrderResponseListener {
 
-    @JmsListener(destination = "LOCAL.REPLY")
+    @JmsListener(destination = "LOCAL.SEND")
     public void receive(Message message) throws JMSException {
         TextMessage textMessage = (TextMessage) message;
-        log.info("### 2 ### Consulta DNI recibe respuesta de RENIEC: {} para el documento: {}",
+        log.info("### 2 ### Se recibe Consulta DNI con el mensaje: {} para el documento: {}",
                 textMessage.getText(), textMessage.getJMSCorrelationID());
 
         // do some business logic here, like updating the order in the database
+    }
+    
+    public void response(String dni) throws JMSException {
+        String queueName = "LOCAL.REPLY";
+        MQQueue orderRequestQueue = new MQQueue(queueName);
+
+        jmsTemplate.convertAndSend(orderRequestQueue, "Se ha procesado la consulta DNI para el documento: "+dni, textMessage -> {
+            textMessage.setJMSCorrelationID(dni);
+            return textMessage;
+        });
+        
+        log.info("### 1 ### Confirmacion de Consulta DNI {} enviada al usuario vía la queue "+queueName, dni);
+
+        return new ResponseEntity(order, HttpStatus.ACCEPTED);
     }
 }
